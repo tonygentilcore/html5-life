c7755fac6d1f2372d73b9d29a1012537678d6ac5
Tony Gentilcore <tonygentilcore@gmail.com>
2019-02-25 15:27:12 -0800

    Disable anti-aliasing. This nearly halves rendering time.

--- life-webgl.js
+++ life-webgl.js
@@ -63,7 +63,7 @@ class GameWebGL {
     canvas.style.width = `${boardSize * cellSize}px`
     canvas.style.height = `${boardSize * cellSize}px`
     container.appendChild(canvas)
-    const context = canvas.getContext('webgl2', { alpha: false })
+    const context = canvas.getContext('webgl2', { alpha: false, antialias: false })
     return context
   }
 
